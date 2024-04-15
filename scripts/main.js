30dafa109d8085e3ca9dea432d910bbeabd4f203
devinans <80769367+devinans@users.noreply.github.com>
2024-04-15 15:48:08 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -15,3 +15,17 @@ window.onbeforeunload = () => {
         form.reset()
     }
 };
+
+var tablinks = document.getElementsByClassName("tab-links"); 
+        var tabconts = document.getElementsByClassName("tab-conts");
+        
+        function opentab(tabname){
+            for(tablink of tablinks){
+                tablink.classList.remove("active-link")
+            }
+            for(tabcont of tabconts){
+                tabcont.classList.remove("active-tab")
+            }
+            event.currentTarget.classList.add("active-link");
+            document.getElementById(tabname).classList.add("active-tab");
+        } 
