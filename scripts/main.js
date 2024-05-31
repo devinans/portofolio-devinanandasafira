ef74e98549f460835bd538636d8c7d9cecbb1ddb
Devina Safira <80769367+devinans@users.noreply.github.com>
2024-06-01 00:55:25 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,28 @@
-const navSlide = () => {
-    const burger = document.querySelector(".burger");
-    const navLists = document.querySelector("nav");
-
-    burger.addEventListener("click", () => {
-        navLists.classList.toggle("nav-active");
-        burger.classList.toggle("toggle-burger");
-    });
-};
-
-navSlide
-
 window.onbeforeunload = () => {
     for (const form of document.getElementsByTagName("form")){
-        form.reset()
+        form.reset();
     }
 };
 
 var tablinks = document.getElementsByClassName("tab-links"); 
-        var tabconts = document.getElementsByClassName("tab-conts");
-        
-        function opentab(tabname){
-            for(tablink of tablinks){
-                tablink.classList.remove("active-link")
-            }
-            for(tabcont of tabconts){
-                tabcont.classList.remove("active-tab")
-            }
-            event.currentTarget.classList.add("active-link");
-            document.getElementById(tabname).classList.add("active-tab");
-        } 
+var tabconts = document.getElementsByClassName("tab-conts");
+    
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link")
+    }
+    for(tabcont of tabconts){
+        tabcont.classList.remove("active-tab")
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+} 
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
